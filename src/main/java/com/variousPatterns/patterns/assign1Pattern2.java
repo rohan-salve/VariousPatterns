b5cdfd0a60c5ec7991576e3260fc3ca46c6cce55
rohan-salve <salvergs9@outlook.com>
2022-12-01 15:26:12 +0530

    Printing row number in pattern

---
+++ src/main/java/com/variousPatterns/patterns/assign1Pattern2.java
@@ -0,0 +1,25 @@
+package com.variousPatterns.patterns;
+/*
+
+1 1 1 1
+2 2 2 2
+3 3 3 3
+4 4 4 4
+
+*/
+
+public class assign1Pattern2 {
+    public static void main(String[] args) {
+        int row, col;
+        int n = 5;
+
+        for (row = 1; row< n ; row++)
+        {
+            for (col = 1 ; col < n ; col++)
+            {
+                System.out.print(row+" ");
+            }
+            System.out.println();
+        }
+    }
+}
